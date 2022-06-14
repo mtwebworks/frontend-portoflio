65079439d2c8b9d65a11e065e0e0445e8f3825a8
mtwebworks <mt.webworks@gmail.com>
2022-06-14 21:52:38 +0200

    style: content correction

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
           <hr className='about__hr' />
         </div>
         <div className='about__text-container about__animation-element'>
-          <p className='about__text'>I&nbsp;am ambitious front-end developer with over 2&nbsp;year of noncommercial expirience and almost 10&nbsp;years of expirience working as graphic designer in creative agencies. I&nbsp;love to design, but i&nbsp;like to bring designs alive even more.<br /><br />I&nbsp;strive for simplicity, clarity and originality in my work through designs where every decision is well thought and implemented to achieve set goals.</p>
+          <p className='about__text'>I&nbsp;am ambitious front-end developer with over 2&nbsp;year of noncommercial expirience and almost 10&nbsp;years of expirience as a graphic designer in creative agencies. I&nbsp;love to design, but i&nbsp;also like to bring designs alive even more.<br /><br />I&nbsp;strive for simplicity, clarity and originality in my work through designs where every decision is well thought and implemented to achieve set goals.</p>
           <a className='about__link about__link--accent' href='./assets/CV_MateuszJakubczyk_EN22.pdf' target="_blank" rel="noopener noreferrer">resume</a>
           <Link className='about__link' to='contact' smooth={true} duration={1000}>contact</Link>
 
@@ -66,7 +66,7 @@ const About = () => {
             &nbsp;scss <span className='about__x'>×</span>
             &nbsp;bem <span className='about__x'>×</span>
             &nbsp;gsap <span className='about__x'>×</span>
-            &nbsp;java script <span className='about__x'>×</span>
+            &nbsp;javascript <span className='about__x'>×</span>
             &nbsp;react <span className='about__x'>×</span>
             &nbsp;mobx <span className='about__x'>×</span>
             &nbsp;redux <span className='about__x'>×</span>
@@ -74,12 +74,12 @@ const About = () => {
             &nbsp;git
           </p>
 
-          <h3 className='about__subtitle'>Tools</h3>
+          <h3 className='about__subtitle'>tools</h3>
           <p className='about__text'>vscode <span className='about__x'>×</span>
             &nbsp;photoshop <span className='about__x'>×</span>
             &nbsp;illustrator</p>
 
-          <h3 className='about__subtitle'>Scocial Links</h3>
+          <h3 className='about__subtitle'>scocial links</h3>
           <p className='about__text'>
             <a className='about__social-link' href='https://www.linkedin.com/in/mtjakubek/' target='_blank'>linkedin</a><span className='about__links-x'>×</span>
             <a className='about__social-link' href='https://github.com/mtwebworks' target='_blank'>github</a><span className='about__links-x'> ×</span>
